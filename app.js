c0605819aa1c03942085e8643a4c0425113badf1
lukenelson2120 <44962634+lukenelson2120@users.noreply.github.com>
2018-11-25 20:53:43 +1100

    Update app.js

--- app.js
+++ app.js
@@ -217,13 +217,13 @@ app.post("/addorder", (req,res) => {
     from: 'muscle.munchies1@gmail.com',
      to: req.body.email,
     subject: 'Muscle Munchies',
-    text: req.body.name + ', Your order is computed and will be ready for pick up at ' + req.body.location + ' when it has been made. An email will be sent when you can pick it up. Total cost will be $' + req.body.price
+    text: 'test' //req.body.name + ', Your order is computed and will be ready for pick up at ' + req.body.location + ' when it has been made. An email will be sent when you can pick it up. Total cost will be $' + req.body.price
   }
    var orderEmail = {
     from: 'muscle.munchies1@gmail.com',
      to: 'muscle.munchies1@gmail.com',
     subject: 'Muscle Munchies',
-    text: 'An order has been made under the name' + req.body.name + ' and will be picked up at ' + req.body.location + '. The total order is made up of '+ req.body.order + 'and costs ' +req.body.price + '. Special requests are as follows ' + req.body.requests +'. For any other info see the order page.' 
+    text: 'test'//'An order has been made under the name' + req.body.name + ' and will be picked up at ' + req.body.location + '. The total order is made up of '+ req.body.order + 'and costs ' +req.body.price + '. Special requests are as follows ' + req.body.requests +'. For any other info see the order page.' 
   }
   console.log(req.body.price);
   newOrder.save()
