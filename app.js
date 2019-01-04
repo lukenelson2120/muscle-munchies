34cc10ac0ff78e123484c0af2a27a872ed07cadf
lukenelson2120 <44962634+lukenelson2120@users.noreply.github.com>
2019-01-04 12:31:56 +1100

    Update app.js

--- app.js
+++ app.js
@@ -273,7 +273,7 @@ res.redirect('/adminF');
 });
 
 app.post("/deleteAll", (req, res) => {
-  order.deleteMany({email:""}, function(err){
+  order.remove({email:""}, function(err){
   if(err) {
     alert(err);
     return;
